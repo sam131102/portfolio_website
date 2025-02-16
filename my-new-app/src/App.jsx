1c1f040f5e9f7ec4e1e7d9a69212836bf8e30ae4
Sambhav Garg <78362105+sam131102@users.noreply.github.com>
2025-02-15 17:05:00 -0800

    Portfolio Updates

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -22,14 +22,14 @@ function App() {
                 className="profile-pic"
               />
               <h2>Sambhav Garg</h2>
-              <h3>Software Developer | Web Developer</h3>
+              <h3>Lead Software Developer | Freelance Web Developer</h3>
               <h4>Turning caffeine into code, I build solutions one line at a time.</h4>
             </div>
             <div className="card about-me-card">
               <div className="card-header">
                 <h2>About Me</h2>
               </div>
-              <p>I am a recent Computer Science graduate from the University of British Columbia, currently residing in Kelowna, BC. I have a strong passion for developing software that solves real-world problems and enhances user experiences. Beyond coding, I enjoy hiking in the beautiful landscapes of British Columbia. I am eager to apply my knowledge and skills in a professional setting, contributing to innovative projects and continuous growth as a developer.</p>
+              <p>I am a Computer Science graduate from the University of British Columbia, currently residing in Kelowna, BC. I have a strong passion for developing software that solves real-world problems and enhances user experiences. Beyond coding, I enjoy hiking in the beautiful landscapes of British Columbia. I am eager to apply my knowledge and skills in a professional setting, contributing to innovative projects and continuous growth as a developer.</p>
               <div className="education">
                 <h3>Education</h3>
                 <p>University of British Columbia (UBC) - Bachelors of Art (Computer Science)</p>
@@ -47,8 +47,8 @@ function App() {
                 <a href="https://github.com/sam131102/Carpooling-App-HCI" className="project-icon" target="_blank" rel="noopener noreferrer">
                   <img src={`${process.env.PUBLIC_URL}/carpool.png`} alt="Carpool" />
                 </a>
-                <a href="https://github.com/sam131102/Data301-GroupProject-IndividualPart" className="project-icon" target="_blank" rel="noopener noreferrer">
-                  <img src={`${process.env.PUBLIC_URL}/fifa.png`} alt="FIFA" />
+                <a href="https://karrentals.ca" className="project-icon" target="_blank" rel="noopener noreferrer">
+                  <img src={`${process.env.PUBLIC_URL}/logo_enhanced.jpg`} alt="KAR" />
                 </a>
                 <a href="https://github.com/sam131102?tab=repositories" className="project-icon" target="_blank" rel="noopener noreferrer">
                   <img src={`${process.env.PUBLIC_URL}/app.png`} alt="App" />
@@ -59,7 +59,31 @@ function App() {
               <div className="card-header">
                 <h2>Work Experience</h2>
               </div>
+              <div className="experience-box">
+                  <h3>Lead Software Developer & Founding Member</h3>
+                  <p>Curious Markings</p>
+                  <p>Nov 2024 - Present</p>
+                  <p className="description">Lead Software Developer at Curious Markings Co., specializing in building intuitive, no-code/low-code platforms like AppSage and MacroSage. Experienced in full-stack development with Ruby, JavaScript, HTML, CSS, and AI-driven solutions using GPT. Passionate about creating scalable, user-friendly applications that empower individuals to design and manage their own digital experiences.</p>
+                </div>
+              <div className="experience-box">
+                  <h3>Rental Agent</h3>
+                  <p>Kelowna Boat Rentals</p>
+                  <p>May 2023 - Sept 2023</p>
+                  <p className="description">Rental Agent with Kelowna Boat Rentals, sales & customer service, maintenance, collection of rental fees and deposits, safety & Training</p>
+                </div>
+              <div className="experience-box">
+                  <h3>Merchandiser</h3>
+                  <p>Costco Wholesale</p>
+                  <p>June 2022 - Nov 2022</p>
+                  <p className="description">Merchandiser Costco, customer interactions, inventory and stocking, sales consulting for tires</p>
+                </div>
               <div className="experience-timeline">
+                <div className="experience-box">
+                  <h3>Tech Advisor</h3>
+                  <p>Best Buy</p>
+                  <p>Sept 2021 - Dec 2021</p>
+                  <p className="description">Best Buy Advisor, sales, customer service, POS system training, training customers on technology solutions</p>
+                </div>
                 <div className="experience-box">
                   <h3>Contract Software Developer</h3>
                   <p>Satya International</p>
@@ -71,24 +95,6 @@ function App() {
                   <p>C.R.I.S</p>
                   <p>July 2019 - Aug 2019</p>
                   <p className="description">Shadowed staff and learned advanced work such as using excel sheet modules for projecting utilization of mobile assets and development of asset utilization for Zonal Railways</p>
-                </div>
-                <div className="experience-box">
-                  <h3>Tech Advisor</h3>
-                  <p>Best Buy</p>
-                  <p>Sept 2021 - Dec 2021</p>
-                  <p className="description">Best Buy Advisor, sales, customer service, POS system training, training customers on technology solutions</p>
-                </div>
-                <div className="experience-box">
-                  <h3>Merchandiser</h3>
-                  <p>Costco Wholesale</p>
-                  <p>June 2022 - Nov 2022</p>
-                  <p className="description">Merchandiser Costco, customer interactions, inventory and stocking, sales consulting for tires</p>
-                </div>
-                <div className="experience-box">
-                  <h3>Rental Agent</h3>
-                  <p>Kelowna Boat Rentals</p>
-                  <p>May 2023 - Sept 2023</p>
-                  <p className="description">Rental Agent with Kelowna Boat Rentals, sales & customer service, maintenance, collection of rental fees and deposits, safety & Training</p>
                 </div>
               </div>
             </div>
